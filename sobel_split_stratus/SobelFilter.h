4e11e4c8ee76ff4d773922595f39297988a442a9
Wingsyu <zx03092005@hotmail.com>
2019-11-11 15:16:22 +0800

    change folder name

---
+++ sobel_split_stratus/SobelFilter.h
@@ -0,0 +1,32 @@
+#ifndef SOBEL_FILTER_H_
+#define SOBEL_FILTER_H_
+#include <systemc>
+using namespace sc_core;
+
+#ifndef NATIVE_SYSTEMC
+#include <cynw_p2p.h>
+#endif
+
+#include "filter_def.h"
+
+class SobelFilter: public sc_module
+{
+public:
+	sc_in_clk i_clk;
+	sc_in < bool >  i_rst;
+#ifndef NATIVE_SYSTEMC
+	cynw_p2p< sc_dt::sc_uint<24> >::in i_rgb;
+	cynw_p2p< sc_dt::sc_uint<32> >::out o_result;
+#else
+	sc_fifo_in< sc_dt::sc_uint<24> > i_rgb;
+	sc_fifo_out< sc_dt::sc_uint<32> > o_result;
+#endif
+
+	SC_HAS_PROCESS( SobelFilter );
+	SobelFilter( sc_module_name n );
+	~SobelFilter();
+private:
+	void do_filter();
+  	int val, total;
+};
+#endif
